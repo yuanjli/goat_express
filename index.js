abbf2cd0a93ed37e0a806cc85a0b333b9ccd5261
yuanjli <liyuanjienetwork@gmail.com>
2018-08-23 11:26:33 -0700

    Bare bone express app set up

---
+++ index.js
@@ -0,0 +1,38 @@
+// Require needed modules
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+
+// Declare app variable
+var app = express();
+
+// Set up use statements
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Define routes 
+app.get('/', function(req, res){
+	res.send('hi from home route');
+})
+
+
+// Listen to the port 3000
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
