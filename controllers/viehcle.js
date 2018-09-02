4ce114dbd6b9ead4013037b9df83a2f70860ceef
yuanjli <liyuanjienetwork@gmail.com>
2018-09-01 23:49:47 -0700

    update for the controllers folder

---
+++ controllers/viehcle.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models'); // add it to the database 
+
+router.get('/', function(req, res){
+	res.send('viehcle');
+});
+
+router.get('/', function(req, res){
+	res.render('authors/new');
+});
+
+router.get('/:id', function(req, res){	// found the author ID
+	//res.send('author show page goes here');
+	res.send('viehcle');
+});
